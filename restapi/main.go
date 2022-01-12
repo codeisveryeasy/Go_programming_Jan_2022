af27977d8bdd84a73923145ff240d1bbc8b5b50f
codeisveryeasy <prafful@airasia.com>
2022-01-12 19:30:28 +0530

    rest with mux

--- restapi/main.go
+++ restapi/main.go
@@ -3,7 +3,11 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"net/http"
+	"strconv"
+
+	"github.com/gorilla/mux"
 )
 
 type Friend struct {
@@ -27,10 +31,18 @@ func main() {
 }
 
 func handleAllRequests() {
-	http.HandleFunc("/", welcome)
+
+	//create instance of type mux router
+	muxRouter := mux.NewRouter().StrictSlash(true)
+	muxRouter.HandleFunc("/", welcome).Methods("GET")
+	muxRouter.HandleFunc("/getallfriends", allFriends).Methods("GET")
+	muxRouter.HandleFunc("/getfriend/{id}", singleFriend).Methods("GET")
+	muxRouter.HandleFunc("/addfriend", addNewFriend).Methods("POST")
+	//http.HandleFunc("/", welcome)
 	//get all friends
-	http.HandleFunc("/getallfriends", allFriends)
-	http.ListenAndServe(":8888", nil)
+	// http.HandleFunc("/getallfriends", allFriends)
+	// http.HandleFunc("/getfriend/id", allFriends)
+	http.ListenAndServe(":8888", muxRouter)
 
 }
 
@@ -43,4 +55,35 @@ func allFriends(response http.ResponseWriter, request *http.Request) {
 	fmt.Println("/getallfriends called!")
 	//marshalling
 	json.NewEncoder(response).Encode(Friends)
+}
+
+func singleFriend(response http.ResponseWriter, request *http.Request) {
+	//fmt.Println(request)
+	fmt.Println(mux.Vars(request))
+	fmt.Println(mux.Vars(request)["id"])
+	receivedMap := mux.Vars(request)
+	receivedId := receivedMap["id"]
+	id, _ := strconv.Atoi(receivedId)
+
+	for _, friend := range Friends {
+		if friend.Id == id {
+			json.NewEncoder(response).Encode(friend)
+		}
+	}
+
+}
+
+func addNewFriend(response http.ResponseWriter, request *http.Request) {
+	//extract request body from request
+	requestBody, _ := ioutil.ReadAll(request.Body)
+	fmt.Println(string(requestBody))
+	//create the instance of type Friend which can be inserted in Friends array
+	var friend Friend
+	//unmarshal
+	json.Unmarshal(requestBody, &friend)
+	//append to Friends
+	Friends = append(Friends, friend)
+
+	json.NewEncoder(response).Encode(Friends)
+
 }
